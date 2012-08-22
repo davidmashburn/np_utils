5cac77fd9213e204bcbc85c5f607fd1e6fc961eb
David N. Mashburn <david.n.mashburn@gmail.com>
2012-08-22 01:25:56 -0500

    Added numpy and scipy as dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='np_utils',
-    version='0.1.1',
+    version='0.1.2',
     author='David N. Mashburn',
     author_email='david.n.mashburn@gmail.com',
     packages=['np_utils'],
@@ -11,5 +11,8 @@ setup(
     license='LICENSE.txt',
     description='',
     long_description=open('README.rst').read(),
-    install_requires=[],
+    install_requires=[
+                      'numpy>=1.0',
+                      'scipy>=0.8',
+                     ],
 )
