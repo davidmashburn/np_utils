1efbf1c14d5d2976972623e4012e61306dfa7f51
David N. Mashburn <david.n.mashburn@gmail.com>
2013-10-08 03:02:53 -0500

    Sum: More additions to func_utils.py, made fork more flexible Details: Added convertToSingleArgFun, convertToMultiArgFun, composem, constf   convertToSingleArgFun / convertToMultiArgFun change whether a function       accepts multiple arguments or a list of arguments   composem(f,g) applies f to (g mapped over all the arguments)   constf creates a constant function from anything
    Moved fork to fork_strict and made fork capable of handling non-callable values
      fork can now handle cases like:
      >>> fork(div,identity,2)
      which would create a "half" function

--- np_utils/func_utils.py
+++ np_utils/func_utils.py
@@ -25,42 +25,37 @@ def mapf(f):
        mapf(f)(x) <--> map(f,x)'''
     return lambda x: map(f,x)
 
+def convertToSingleArgFun(f):
+    '''Take a function that takes multiple arguments and create a function
+       that takes a single argument with multiple values'''
+    return lambda x,**kwds: f(*x,**kwds)
+
+def convertToMultiArgFun(f):
+    '''Take a function that takes a single argument with multiple values
+       and create a function that takes a multiple arguments'''
+    return lambda *args,**kwds: f(args,**kwds)
+
 def compose(*functions):
     '''A compose function for python, i.e.:
        
        compose(f1,f2,f3)(x) <--> f1(f2(f3(x)))
        
        Works best with single-argument functions, but the last function
-       can take any kind of arguments. ( No arrows yet ;-) )'''
+       can take any kind of arguments, i.e.:
+       
+       compose(f1,f2,f3)(x,y,z) <--> f1(f2(f3(x,y,z)))'''
     if len(functions)==1:
         return functions[0]
     else:
         return lambda *x,**kwds: functions[0](compose(*functions[1:])(*x,**kwds))
 
-def fork(f,*functionList):
-    '''A generalized fork function for python.
-       Returns a new function that has the property:
-         fork(a,b,c,d,...)(x,y,...) <--> a(b(x,y,...),c(x,y,...),d(x,y,...),...)
-       The generated function can be read naturally as:
-         "Do a to the results of b,c,d,... applied separately to something"
+def composem(f,g):
+    '''A mapping compose function for python, i.e.:
        
-       An example, (assuming you have imported "div" from the operator module):
-       >>> mean = fork(div,sum,len)
+       compose(f,g)(x,y,z,...) <--> f(g(x),g(y),g(z),...)
        
-       which reads: "Divide the sum of something by its length"
-       
-       This generated function is equivalent to:
-       >>> def mean(*args):
-       >>>    return div(sum(*args),len(*args))
-       
-       This concept is derived from Ken Iverson's J language:
-         http://www.jsoftware.com/
-       Note, however, that the order of application is prefix instead of infix.
-       Aka, the same mean function in J would be:
-           mean =: sum div len
-       
-       In the simplest case, fork(a,b) <--> compose(a,b)'''
-    return lambda *args,**kwds: f(*[i(*args,**kwds) for i in functionList])
+       keyword arguments go to the g function (same as compose)'''
+    return lambda *args,**kwds: f(*[g(i,**kwds) for i in args])
 
 def constf(value):
     '''Make a function that takes anything but always returns the value given here.
@@ -70,3 +65,49 @@ def constf(value):
            f(14) -> 10
            f(1,5,a=6) -> 10'''
     return lambda *args,**kwds: value
+
+def fork(f,*functionOrValuesList):
+    '''A generalized fork function for python which additionally allows
+           non-callable values to be passed through.
+       
+       Takes a function, any number of additional functions.
+       Returns a new function that has the property:
+         fork(a,b,c,d,...)(x,y,...) <--> a(b(x,y,...),c(x,y,...),d(x,y,...),...)
+       or if an item in the list is not a function (for instance, c below):
+         fork(a,b,c,d,...)(x,y,...) <--> a(b(x,y,...),c,d(x,y,...),...)
+       The generated function can be read naturally as:
+         "Do a to the results of b,c,d,... applied separately to something"
+       
+       An example, (assuming you have imported "div" from the operator module):
+       >>> mean = fork(div,sum,len)
+       This reads "Divide the sum of something by its length"
+       
+       This generated function is equivalent to:
+       >>> def mean(*args):
+       >>>    return div(sum(*args),len(*args))
+       
+       Or, using a non-callable:
+       >>> mean = fork(div,sum,2)
+       This reads: "Divide the sum of something by 2"
+       
+       This concept is derived from Ken Iverson's J language:
+         http://www.jsoftware.com/
+       Note, however, that the order of application is prefix instead of infix.
+       Aka, the same mean function in J would be:
+           mean =: sum div len
+       
+       In the simplest case with b as a function, fork is equivalent to "compose":
+           fork(a,b) <--> compose(a,b)
+       Alternatively, with b as a value, fork is equivalent to "call"
+           fork(a,b) <--> a(b)'''
+    return lambda *args,**kwds: f(*[ ( i(*args,**kwds)
+                                       if hasattr(i,'__call__') else
+                                       i )
+                                    for i in functionOrValuesList])
+
+def fork_strict(f,*functionList, **forkKwds):
+    '''The same as fork, but restricted to functions only.
+       
+       Here is the documentation for fork:
+       '''+fork.__doc__
+    return lambda *args,**kwds: f(*[i(*args,**kwds) for i in functionList])
